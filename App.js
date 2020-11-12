dde32b97c9f1eed6e13757f72cf4ec8d87b1880a
Bharath Gowda <bhojaraj.gouda@gmail.com>
2020-11-13 01:27:19 +0530

    Creating new components for specific items

--- App.js
+++ App.js
@@ -1,40 +1,25 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
+import { StyleSheet, View, FlatList } from 'react-native';
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
-  const [ enteredGoal, setEnteredGoal ] = useState(''),
-  [ goalList, setGoalList ] = useState([]);
+  const [ courseGoals, setCourseGoals ] = useState([]);
 
-  const goalInputHandler = text => {
-    setEnteredGoal(text);
-  };
-
-  const addGoalHandler = () => {
-    setGoalList(currentGoals => [
+  const addGoalHandler = goalTitle => {
+    setCourseGoals(currentGoals => [
       ...currentGoals, 
-      { id: Math.random().toString(), value: enteredGoal }
+      { id: Math.random().toString(), value: goalTitle }
     ]);
   };
 
   return (
-    <View style={styles.container}>
-        <View style={styles.inputContainer}>
-          <TextInput 
-            placeholder="Goal" 
-            style={styles.input} 
-            onChangeText={goalInputHandler} 
-            value={enteredGoal}
-          />
-          <Button title="ADD" onPress={addGoalHandler}/>
-        </View>
+    <View style={ styles.container }>
+        <GoalInput onAddGoal={ addGoalHandler } />
         <FlatList 
-          keyExtractor={(item, index) => item.id} 
-          data={goalList} 
-          renderItem={itemData => (
-            <View style={styles.goalList}>
-              <Text>{itemData.item.value}</Text>
-            </View>
-          )} 
+          keyExtractor={ (item, index) => item.id } 
+          data={ courseGoals } 
+          renderItem={ itemData => <GoalItem title={ itemData.item.value } /> } 
         />
     </View>
   );
@@ -43,24 +28,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     padding: 50
-  },
-  inputContainer: {
-    flexDirection: 'row', 
-    justifyContent: 'space-between', 
-    alignItems: 'center'
-  },
-  input: {
-    width: '80%', 
-    borderColor:'#000', 
-    borderWidth: 1, 
-    padding: 10
-  },
-  goalList: {
-    padding: 10,
-    borderColor: '#000000',
-    backgroundColor: '#CCCCCC',
-    borderWidth: 1,
-    marginVertical: 10
   }
-
 });
